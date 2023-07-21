dc6ac99252d0a3c143deabd79b029bb0b9966e6c
nabanitab <nabanitabapari@ca-one.com>
2023-07-21 15:57:51 +0530

    BasicSumOfTwoNum

---
+++ src/main/java/array/BasicSumOfTwoNum.java
@@ -0,0 +1,11 @@
+package array;
+
+public class BasicSumOfTwoNum {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
